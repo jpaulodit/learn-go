31ab800a2c3110263d0577a25ced9ef5b1f1cac6
John Win <jpaulodit@hotmail.com>
2018-05-07 23:26:01 -0700

    Interfaces lesson.
    Interface lesson and assignments.

---
+++ interface_assignment2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "io"
+)
+
+func main()  {
+    f := os.Args[1]
+    fmt.Println("File to open is:", f)
+
+    my_file, err := os.Open(f)
+
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+
+    io.Copy(os.Stdout, my_file)
+}
---
+++ interfaces_http/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    "os"
+    "io"
+)
+
+type logWriter struct {}
+
+func main()  {
+    resp, err := http.Get("http://google.com")
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+
+    //bs := make([]byte, 9999)
+    //if n, err := resp.Body.Read(bs); err != nil {
+    //    fmt.Println("Error:", err)
+    //} else {
+    //    fmt.Println(string(bs))
+    //    fmt.Println(n, "bytes were read.")
+    //}
+    //
+    //// same as above.
+    //io.Copy(os.Stdout, resp.Body)
+
+    lw := logWriter{}
+
+    io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+    fmt.Println(string(bs))
+    return len(bs), nil
+}
